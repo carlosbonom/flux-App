96d34c52c72e4c082a69d4724206edb226eba3c4
carlosbonom <carlos_bonomelli@outlook.com>
2020-12-09 09:53:34 -0300

    estilos botonera y caja

---
+++ src/components/Botonera.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Botonera.css'
+
+const Botonera = () =>{
+
+    return(
+        <div className="Botonera">
+            <button>Rojo</button>
+            <button>Verde</button>
+
+        </div>
+    )
+
+}
+
+export default Botonera
---
+++ src/components/Caja.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Caja.css'
+
+const Caja = () =>{
+
+    return(
+        <div className="Caja">
+            <div className="color-container"></div>
+        </div>
+    )
+
+}
+
+export default Caja
